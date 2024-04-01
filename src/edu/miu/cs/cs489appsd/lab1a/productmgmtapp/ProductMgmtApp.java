46662f5585bb5eab8f4e13fc76817b811d8ee1c1
kash <parakash.sbd@gmail.com>
2024-04-01 18:01:44 -0500

    lab1a is done

--- src/edu/miu/cs/cs489appsd/lab1a/productmgmtapp/ProductMgmtApp.java
+++ src/edu/miu/cs/cs489appsd/lab1a/productmgmtapp/ProductMgmtApp.java
@@ -1,19 +1,53 @@
 package edu.miu.cs.cs489appsd.lab1a.productmgmtapp;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import edu.miu.cs.cs489appsd.lab1a.productmgmtapp.model.Product;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class ProductMgmtApp {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Product[] products = {
+                new Product("3128874119", "Banana", "2023-01-24", 124, 0.55),
+                new Product("2927458265", "Apple", "2022-12-09", 18, 1.09),
+                new Product("9189927460", "Carrot", "2023-03-31", 89, 2.99),
+        };
+        printProducts(products);
+    }
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void printProducts(Product[] products) {
+        List<Product> sortedProducts = Arrays.stream(products).sorted(Comparator.comparing(Product::getName)).toList();
+        printJSON(sortedProducts);
+        System.out.println("------------------------------------");
+        printXML(sortedProducts);
+        System.out.println("------------------------------------");
+        printCSV(sortedProducts);
+    }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    public static void printJSON(List<Product> sortedProducts) {
+        System.out.println("Printed in JSON Format");
+        System.out.println("[");
+        for (Product product : sortedProducts) {
+            System.out.println("{ \"productId\":" + product.getProductId() + ", \"name\":" + product.getName() + ", \"dateSupplied\":" + product.getDateSupplied() + ", \"quantityInStock\":" + product.getQuantityInStock() + ", \"unitPrice\":" + product.getUnitPrice() + "  },");
+        }
+        System.out.println("]");
+    }
+
+    public static void printXML(List<Product> sortedProducts) {
+        System.out.println("Print XML Format");
+        System.out.println("<?xml version=\"1.0\"");
+        System.out.println("<products>");
+        for (Product product : sortedProducts) {
+            System.out.println("<product productId=\"" + product.getProductId() + "\" name=\"" + product.getName() + "\" dateSupplied=\"" + product.getDateSupplied() + "\" quantityInStock=\"" + product.getQuantityInStock() + "\" unitPrice=\"" + product.getUnitPrice() + "\"/>");
+        }
+        System.out.println("</products>");
+    }
+
+    public static void printCSV(List<Product> sortedProducts) {
+        System.out.println("Print Comma-Seperated Value - CSV Format");
+        for (Product product : sortedProducts) {
+            System.out.println(product.getProductId() + ", " + product.getName() + ", " + product.getDateSupplied() + ", " + product.getQuantityInStock() + ", " + product.getUnitPrice());
         }
     }
 }
--- src/edu/miu/cs/cs489appsd/lab1a/productmgmtapp/model/Product.java
+++ src/edu/miu/cs/cs489appsd/lab1a/productmgmtapp/model/Product.java
@@ -1,5 +1,92 @@
-package edu.miu.cs.cs489appsd.lab1a.productmgmtapp;
+package edu.miu.cs.cs489appsd.lab1a.productmgmtapp.model;
 
+import java.util.Objects;
 public class Product {
-    
+    private String productId;
+    private String name;
+    private String dateSupplied;
+    private int quantityInStock;
+    private double unitPrice;
+
+    public Product() {
+
+    }
+
+    public Product(String productId, String name, String dateSupplied, int quantityInStock, double unitPrice) {
+        this.productId = productId;
+        this.name = name;
+        this.dateSupplied = dateSupplied;
+        this.quantityInStock = quantityInStock;
+        this.unitPrice = unitPrice;
+    }
+
+    public Product(String name, String dateSupplied, int quantityInStock, double unitPrice) {
+        this.name = name;
+        this.dateSupplied = dateSupplied;
+        this.quantityInStock = quantityInStock;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateSupplied() {
+        return dateSupplied;
+    }
+
+    public void setDateSupplied(String dateSupplied) {
+        this.dateSupplied = dateSupplied;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return Objects.equals(getProductId(), product.getProductId()) && getQuantityInStock() == product.getQuantityInStock() && Double.compare(product.getUnitPrice(), getUnitPrice()) == 0 && Objects.equals(getName(), product.getName()) && Objects.equals(getDateSupplied(), product.getDateSupplied());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getProductId(), getName(), getDateSupplied(), getQuantityInStock(), getUnitPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", dateSupplied='" + dateSupplied + '\'' +
+                ", quantityInStock=" + quantityInStock +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
 }
